db2c19a397e6c6654f86c8a7349117b0b3a9d3ac
trung <thanhtrung3802@gmail.com>
2023-12-19 17:23:04 +0700

    1
    Signed-off-by: trung <thanhtrung3802@gmail.com>

--- src/app/components/client/email-verification/email-verification.component.ts
+++ src/app/components/client/email-verification/email-verification.component.ts
@@ -20,20 +20,33 @@ export class EmailVerificationComponent {
       .set('email', this.email)
       .set('otp', this.otp);
   
-    // Gửi yêu cầu đến backend
-    this.emailService.verifyEmailAndOTP(params).subscribe(
-      response => {
-        // Xác minh thành công, hiển thị thông báo và chuyển hướng
-        console.log('Xác minh thành công:', response);
-        // Hiển thị thông báo (có thể sử dụng toastr hoặc cách hiển thị thông báo tùy chọn)
-        alert('Xác minh thành công!');
-        // Chuyển hướng đến trang đăng nhập
-        this.router.navigate(['/login']);
+    // // Gửi yêu cầu đến backend
+    // this.emailService.verifyEmailAndOTP(params).subscribe(
+    //   response => {
+    //     // Xác minh thành công, hiển thị thông báo và chuyển hướng
+    //     console.log('Xác minh thành công:', response);
+    //     // Hiển thị thông báo (có thể sử dụng toastr hoặc cách hiển thị thông báo tùy chọn)
+    //     alert('Xác minh thành công!');
+    //     // Chuyển hướng đến trang đăng nhập
+    //     this.router.navigate(['/login']);
+    //   },
+    //   error => {
+    //     // Xử lý lỗi nếu có
+    //     console.error('Lỗi xác minh:', error);
+    //   }
+    // );
+    this.emailService.verifyEmailAndOTP(params).subscribe({
+      next: res => {
+          console.log('Xác minh thành công:', res);
+          // Hiển thị thông báo (có thể sử dụng toastr hoặc cách hiển thị thông báo tùy chọn)
+          alert('Xác minh thành công!');
+          // Chuyển hướng đến trang đăng nhập
+          this.router.navigate(['/login']);
       },
-      error => {
-        // Xử lý lỗi nếu có
-        console.error('Lỗi xác minh:', error);
+      error: error => {
+          console.error('Lỗi xác minh:', error);
+
       }
-    );
+  })
   }
 }
